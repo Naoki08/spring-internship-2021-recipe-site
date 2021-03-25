30fe8b72a5d34c238af2dcebc5be166e3674e34e
Naoki <36870844+Naoki08@users.noreply.github.com>
2021-03-25 22:57:35 +0900

    _app.tsxと_document.tsxを追加して共通documentを定義

--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-import '../data/style.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-
-export default function MyApp({ Component, pageProps}) {
-    return <Component {...pageProps} />
-}
